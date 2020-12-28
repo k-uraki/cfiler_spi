bc49bd20f74f338d3c2d56ddaa4b48880f823bf9
k-uraki <k-uraki@din.or.jp>
2020-12-29 00:47:25 +0900

    x64アーキテクチャビルド ※ビルドはしたものの、32bit SPI(DLL)がロードできないのでダメなはず

--- src/vcmake.bat
+++ src/vcmake.bat
@@ -1,9 +1,12 @@
 REM for VS2005
 REM cygwin1.7‚Å‚Ìƒrƒ‹ƒh•û–@‚ª‚í‚©‚ç‚È‚¢‚½‚ß
 
+SET ARCH=x64
+
 REM call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
 
 @echo on
 
@@ -19,12 +22,17 @@ REM SET PYTHON_ROOT=%USERPROFILE%\AppData\Local\Programs\Python\Python37-32
 REM SET PYTHON_LIB_FILENAME=python37.lib
 
 REM Python 3.8
-SET PYTHON_ROOT=C:\Python38-32
+REM SET PYTHON_ROOT=C:\Python38-32
+REM SET PYTHON_LIB_FILENAME=python38.lib
+
+REM Python 3.8 64bit
+SET PYTHON_ROOT=C:\Python38
 SET PYTHON_LIB_FILENAME=python38.lib
 
 cl /D "WIN32" /D "NDEBUG" /D "_ATL_MIN_CRT" /D "_UNICODE" /D "UNICODE" /FD /EHsc /MT /GR- /c /TP /I%PYTHON_ROOT%\include spi.cpp /Fospi.obj
 
-link /INCREMENTAL:NO /NOLOGO /LIBPATH:%PYTHON_ROOT%\libs /MACHINE:X86 /DLL /OUT:spi.pyd %PYTHON_LIB_FILENAME% spi.obj
+REM link /INCREMENTAL:NO /NOLOGO /LIBPATH:%PYTHON_ROOT%\libs /MACHINE:X86 /DLL /OUT:spi.pyd %PYTHON_LIB_FILENAME% spi.obj
+link /INCREMENTAL:NO /NOLOGO /LIBPATH:%PYTHON_ROOT%\libs /MACHINE:%ARCH% /DLL /OUT:spi.pyd %PYTHON_LIB_FILENAME% spi.obj
 
 del *.lib
 del *.exp
